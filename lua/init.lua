fd1530ec77727b01800300a63d4a29c09db5e1f9
Colton Padden <colton.padden@fastmail.com>
2022-09-26 13:38:42 -0400

    format countdown timer, add ability to skip interval

--- lua/init.lua
+++ lua/init.lua
@@ -1,12 +1,21 @@
--- PomdoroStart
--- PomodoroStop
+local M = { work_duration = 25 * 60, break_duration = 5 * 60, counter = nil, timer = nil, state = nil, previous_state = nil }
 
-local M = { work_duration = 3, break_duration = 2, counter = nil, timer = nil }
-
-local STATE_PAUSED = 0
-local STATE_WORKING = 1
+local STATE_WORKING = 0
+local STATE_PAUSED = 1
 local STATE_BREAK = 2
 
+local STATE_MESSAGE_MAPPING = {
+    [STATE_WORKING] = "Working",
+    [STATE_PAUSED] = "Paused",
+    [STATE_BREAK] = "Break",
+}
+
+local function format_seconds(seconds)
+    local hh = string.format("%02.f", math.floor(seconds / 3600))
+    local mm = string.format("%02.f", math.floor(seconds / 60 - (hh * 60)))
+    local ss = string.format("%02.f", math.floor(seconds - hh * 3600 - mm * 60))
+    return hh .. ":" .. mm .. ":" .. ss
+end
 
 function M.start_timer(callback)
     M.timer = vim.loop.new_timer()
@@ -27,28 +36,33 @@ function M.stop_timer()
 end
 
 function M.start()
-    -- TODO - determine the previous state; un-pausing should resume where things left
-    -- off
-    M.state = STATE_WORKING
+    if M.previous_state ~= nil then
+        M.state = M.previous_state
+    else
+        M.state = STATE_WORKING
+    end
     M.start_timer(M.event_loop)
 end
 
 function M.pause()
+    M.previous_state = M.state
     M.state = STATE_PAUSED
 end
 
-function M.render_popup_content()
-    local msg = ""
+function M.skip()
     if M.state == STATE_WORKING then
-        msg = "Keep up the good work! There are " .. tostring(M.counter) .. " seconds remaining in this interval."
+        M.state = STATE_BREAK
+        M.counter = M.break_duration
     elseif M.state == STATE_BREAK then
-        msg = "There are " .. tostring(M.counter) .. " seconds remaining in your break."
-    elseif M.state == STATE_PAUSED then
-        msg = "Paused..."
+        M.state = STATE_WORKING
+        M.counter = M.work_duration
     end
+end
+
+function M.render_popup_content()
     return {
-        msg,
-        "Counter " .. tostring(M.counter) .. " seconds",
+        " Status: " .. STATE_MESSAGE_MAPPING[M.state],
+        " " .. format_seconds(M.counter) .. " seconds remain",
     }
 end
 
@@ -78,12 +92,13 @@ function M.init()
     M.window = require("pomodoro.window")
 
     M.window.title = "pomodoro.nvim"
-    M.window.subtitle = "[q]uit [s]tart [p]ause"
+    M.window.subtitle = "[q]uit [s]tart [p]ause s[k]ip"
 
     M.window.mappings = {
-        ['q'] = M.window.close_window,
-        ['s'] = M.start,
-        ['p'] = M.pause
+        ["q"] = M.window.close_window,
+        ["s"] = M.start,
+        ["p"] = M.pause,
+        ["k"] = M.skip,
     }
 
     M.window.open_window()
